b40bc359b04349ba2633b86295bdde8e4adbf7a1
hnguyen16 <bobby_n475@yahoo.com>
2017-07-12 21:42:11 -0500

    add restauranthour DM

---
+++ FriendlyMenu/DataModels/RestaurantHourDM.cs
@@ -0,0 +1,9 @@
+﻿namespace DataModels
+{
+    public class RestaurantHourDM
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public int SharedHourId { get; set; }
+    }
+}
