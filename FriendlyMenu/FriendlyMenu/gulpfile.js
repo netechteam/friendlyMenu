5ef8c47ec8d001ad2ce9a5dcda65afa9f5efba71
YellowDick <hnguyen16@gmail.com>
2017-09-24 15:26:21 -0500

    fix structure of css directory.  add gulp-rename.  create scss partials for home page and list page.

--- FriendlyMenu/FriendlyMenu/gulpfile.js
+++ FriendlyMenu/FriendlyMenu/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding ProjectOpened='watch:sass' />
+﻿/// <binding BeforeBuild='compile:sass' ProjectOpened='watch:sass' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
@@ -6,17 +6,19 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 var webroot = "./wwwroot/";
 var paths = {
     scss: webroot + "css/**/*.scss",
-    scssDest: webroot + "css/" 
+    scssDest: webroot + "css/"
 }
 gulp.task('compile:sass', function () {
     // place code for your default task here
-    gulp.src(paths.scss)
-        .pipe(sass())
-        .pipe(gulp.dest(paths.scssDest))
+    gulp.src(paths.scss, { base: "./"})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest(webroot + "/css/"))
 });
 
 gulp.task("compile", ["compile:sasss"]);
